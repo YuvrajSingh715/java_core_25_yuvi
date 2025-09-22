899a808310756a696f22230bfd668da1230967c7
Yuvraj Singh Kushwah <kushwahyuvraj262@gmail.com>
2025-09-22 17:16:44 +0530

    my increment and decrement oprator demo

--- src/com/persistent/operators/IncrementOperatorDemo.java
+++ src/com/persistent/operators/IncrementOperatorDemo.java
@@ -4,15 +4,19 @@ public class IncrementOperatorDemo {
 
 	public static void main(String[] args) {
 
+		// PreIncrement Operator Demo
 		int x = 2;
 		int y = x++;
 
+		System.out.println(x+" and "+y);
 		System.out.println("final value of x = " + x);
 		System.out.println("final value of y = " + y);
-
+  
+		// PostIncrement Operator Demo
 		int a = 3;
 		int b = ++a;
 
+		System.out.println(x+" and "+y);
 		System.out.println("final value of x = " + a);
 		System.out.println("final value of y = " + b);
 	}
