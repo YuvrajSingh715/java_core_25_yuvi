9b28a53bc84840bbd8b89200f64acc54f6cb28a0
Yuvraj Singh Kushwah <kushwahyuvraj262@gmail.com>
2025-09-25 18:09:31 +0530

    I have added some Oprerator related demo file

---
+++ src/Mock_interview/Student1.java
@@ -0,0 +1,24 @@
+package Mock_interview;
+
+public class Student1 {
+
+	public int rollNumber;
+	public String studentName;
+
+	public int getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+}
--- src/Mock_interview/Student_Test.java
+++ src/Mock_interview/Student_Test.java
@@ -3,6 +3,7 @@ package Mock_interview;
 public class Student_Test {
 
 	public static void main(String[] args) {
+		
 		Student test = new Student(555, "Yuvraj Singh");
 		
 		System.out.println("id number = " + test.getId());
