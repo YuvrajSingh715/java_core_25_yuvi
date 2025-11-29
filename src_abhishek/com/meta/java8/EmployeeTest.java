aea3b09c346d2f38e1da369c2f4bc62945dff4c8
Abhishek Singh Kushwah <abhisheksinghk737@gmail.com>
2025-11-29 14:57:20 +0530

    added employee query logic java8

---
+++ src_abhishek/com/meta/java8/EmployeeTest.java
@@ -0,0 +1,78 @@
+package com.meta.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+
+		List<Employee> employeeList = new ArrayList<Employee>();
+
+		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+		employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
+		employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
+		employeeList.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
+		employeeList.add(new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0));
+		employeeList.add(new Employee(177, "Manu Sharma", 35, "Male", "Account And Finance", 2010, 27000.0));
+		employeeList.add(new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0));
+		employeeList.add(new Employee(199, "Amelia Zoe", 24, "Female", "Sales And Marketing", 2016, 11500.0));
+		employeeList.add(new Employee(200, "Jaden Dough", 38, "Male", "Security And Transport", 2015, 11000.5));
+		employeeList.add(new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0));
+		employeeList.add(new Employee(222, "Nitin Joshi", 25, "Male", "Product Development", 2016, 28200.0));
+		employeeList.add(new Employee(233, "Jyothi Reddy", 27, "Female", "Account And Finance", 2013, 21300.0));
+		employeeList.add(new Employee(244, "Nicolus Den", 24, "Male", "Sales And Marketing", 2017, 10700.5));
+		employeeList.add(new Employee(255, "Ali Baig", 23, "Male", "Infrastructure", 2018, 12700.0));
+		employeeList.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
+		employeeList.add(new Employee(277, "Anuj Chettiar", 31, "Male", "Product Development", 2012, 35700.0));
+
+		employeeList.stream().map(Employee::getDepartment).distinct().forEach(System.out::println);
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Map<String, Long> noOfMaleAndFemaleEmployees = employeeList.stream()
+				.collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+		System.out.println(noOfMaleAndFemaleEmployees);
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Map<String, Double> avgAgeOfMaleAndFemaleEmployees = employeeList.stream()
+				.collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
+
+		System.out.println(avgAgeOfMaleAndFemaleEmployees);
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Optional<Employee> highestPaidEmployeeWrapper = employeeList.stream()
+				.collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)));
+
+		Employee highestPaidEmployee = highestPaidEmployeeWrapper.get();
+		System.out.println(highestPaidEmployee.getId());
+		System.out.println(highestPaidEmployee.getSalary());
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+		employeeList.stream().filter(e -> e.yearOfJoining > 2015).map(Employee::getName).forEach(System.out::println);
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Map<String, Long> employeeCountByDepartment = employeeList.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+		System.out.println(employeeCountByDepartment);
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		// Map<String, Long> map2ndHghstSalaryDptWise =
+		// employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment,Collectors.collectingAndThen(Collectors.toList(),empList
+		// ->empList.stream().map(Employee::getSalary).sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(0L))));
+		Map<String, Object> map2ndHghstSalaryDptWise = employeeList.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment,
+						Collectors.collectingAndThen(Collectors.mapping(Employee::getSalary, Collectors.toList()),
+								list -> list.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst()
+										.orElse((double) 0L))));
+		System.out.println(map2ndHghstSalaryDptWise);
+
+		System.out.println(
+				"+++++++++++++++++++++++++++old age person with department+++++++++++++++++++++++++++++++++++++++++");
+
+		Optional<Employee> Oldage = employeeList.stream().max(Comparator.comparingInt(Employee::getAge));
+
+		String nameDepartment = Oldage.map(Employee::getDepartment).orElse("No Employee");
+		System.out.println("Employee Name = " +Oldage.get());
+	}
+
+}
