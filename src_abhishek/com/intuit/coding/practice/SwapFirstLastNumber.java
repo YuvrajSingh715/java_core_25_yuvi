439c6769c70ada75efd086369b719356e0c2746e
Abhishek Singh Kushwah <abhisheksinghk737@gmail.com>
2025-12-09 13:44:21 +0530

    added application.yml

---
+++ src_abhishek/com/intuit/coding/practice/SwapFirstLastNumber.java
@@ -0,0 +1,20 @@
+package com.intuit.coding.practice;
+class SwapFirstLastNumber{
+	
+	public static void main (String[] args){
+
+       int num = ***REMOVED***;
+
+      String str = String.valueOf(num);	
+
+      char first = str.charAt(0);
+      char  last = str.charAt(str.length()-1);
+
+      String middle = str.substring(1,str.length()-1);	
+
+      String result = ""+last+middle+first;
+      System.out.println(result);	
+
+     }
+
+}
